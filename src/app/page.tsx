e10b97f0fbd5fa3c418834be6ca5c9dd72acf854
Joseph <sejokarizz@gmail.com>
2023-10-25 19:37:45 +0300

    add all css colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import ColorCard from '@/components/ColorCard';
 
 
-const colorPattern = /#(?:[0-9a-fA-F]{3,6})|\b(red|blue|green|yellow|purple|pink|orange|black|cyan|white|gray|violet|indigo)\b|rgba?\([^)]*\)|hsla?\([^)]*\)/gi;
+const colorPattern = /#(?:[0-9a-fA-F]{3,6})|\b(AliceBlue|AntiqueWhite|Aqua|Aquamarine|Azure|Beige|Bisque|Black|BlanchedAlmond|Blue|BlueViolet|Brown|BurlyWood|CadetBlue|Chartreuse|Chocolate|Coral|CornflowerBlue|Cornsilk|Crimson|Cyan|DarkBlue|DarkCyan|DarkGoldenRod|DarkGray|DarkGrey|DarkGreen|DarkKhaki|DarkMagenta|DarkOliveGreen|DarkOrange|DarkOrchid|DarkRed|DarkSalmon|DarkSeaGreen|DarkSlateBlue|DarkSlateGray|DarkSlateGrey|DarkTurquoise|DarkViolet|DeepPink|DeepSkyBlue|DimGray|DimGrey|DodgerBlue|FireBrick|FloralWhite|ForestGreen|Fuchsia|Gainsboro|GhostWhite|Gold|GoldenRod|Gray|Grey|Green|GreenYellow|HoneyDew|HotPink|IndianRed|Indigo|Ivory|Khaki|Lavender|LavenderBlush|LawnGreen|LemonChiffon|LightBlue|LightCoral|LightCyan|LightGoldenRodYellow|LightGray|LightGrey|LightGreen|LightPink|LightSalmon|LightSeaGreen|LightSkyBlue|LightSlateGray|LightSlateGrey|LightSteelBlue|LightYellow|Lime|LimeGreen|Linen|Magenta|Maroon|MediumAquaMarine|MediumBlue|MediumOrchid|MediumPurple|MediumSeaGreen|MediumSlateBlue|MediumSpringGreen|MediumTurquoise|MediumVioletRed|MidnightBlue|MintCream|MistyRose|Moccasin|NavajoWhite|Navy|OldLace|Olive|OliveDrab|Orange|OrangeRed|Orchid|PaleGoldenRod|PaleGreen|PaleTurquoise|PaleVioletRed|PapayaWhip|PeachPuff|Peru|Pink|Plum|PowderBlue|Purple|RebeccaPurple|Red|RosyBrown|RoyalBlue|SaddleBrown|Salmon|SandyBrown|SeaGreen|SeaShell|Sienna|Silver|SkyBlue|SlateBlue|SlateGray|SlateGrey|Snow|SpringGreen|SteelBlue|Tan|Teal|Thistle|Tomato|Turquoise|Violet|Wheat|White|WhiteSmoke|Yellow|YellowGreen)\b|rgba?\([^)]*\)|hsla?\([^)]*\)/gi;
 
 export default function Home() {
   const [inputValue, setInputValue] = useState('');
@@ -31,23 +31,23 @@ export default function Home() {
 
   return (
     <div className='grid  grid-rows-2'>
-      <div className="flex items-center min-h-48">
+      <div className="flex items-center mt-20">
         <div className="mx-auto">
           <input
             type="text"
             placeholder="Type here"
-            className="input input-bordered max-w-xl"
+            className="input input-bordered rounded-sm max-w-xl"
             value={inputValue}
             onChange={handleInputChange}
           />
-          <button className="btn btn-outline btn-info" onClick={handleSearch}>
+          <button className="btn btn-outline btn-info rounded-sm" onClick={handleSearch}>
             Lets get Mixing
           </button>
         </div>
       </div>
 
       {colorTokens.length >= 1 && (
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 p-4">
+        <div className="mt-2 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 p-4">
           {colorTokens.map((token, index) => (
             <ColorCard key={index} color={token} onRemove={() => removeColor(token)} />
           ))}
--- src/components/ColorCard.tsx
+++ src/components/ColorCard.tsx
@@ -1,27 +1,60 @@
 "use client"
 
 import React from 'react';
+import chroma from 'chroma-js';
+
 
 const ColorCard = ({ color, onRemove }) => {
   const cardStyle = {
     backgroundColor: color,
   };
 
+  const oppositeColor = isBlackColor(color) ? '#ffffff' : calculateOppositeColor(color);
+
+  const textStyles = {
+    color: oppositeColor, // Set text color to the opposite color
+  };
+
+  const buttonStyle = {
+    backgroundColor: oppositeColor, // Set button background color to the opposite color
+  };
+
+  function calculateOppositeColor(cssColor) {
+    try {
+      chroma
+      const chromaColor = chroma(cssColor);
+      // Calculate the opposite color by inverting
+      const invertedColor = chromaColor.luminance() > 0.5 ? chromaColor.darken() : chromaColor.brighten();
+      // Convert to a CSS color string
+      return invertedColor.css();
+    } catch (error) {
+      console.error('Error calculating opposite color:', error);
+      return cssColor; // Default to returning the input color on error
+    }
+  }
+  
+
+  function isBlackColor(hexColor) {
+    // Check if the color is black
+    return hexColor === '#000000';
+  }
+
   return (
-    <div className="card w-40 min-h-16 relative" style={cardStyle}>
+    <div className="flex flex-row-reverse card w-40 min-h-16 relative rounded-md border" style={cardStyle}>
       <button
-        className="absolute top-1 right-1 p-2 rounded-full bg-glass cursor-pointer"
+        className="glass p-2 rounded-sm cursor-pointer"
+        
         onClick={() => onRemove(color)}
       >
         X
       </button>
       <div className="card-body">
-        <h2 className="card-title text-center">{color}</h2>
+        <p className="card-title text-center" style={textStyles}>
+          {color}
+        </p>
       </div>
     </div>
   );
 }
 
 export default ColorCard;
-
-// here what i want to do is once a color prop is reveived the components background color is set to that color and the name of that color or code set in the h tag
