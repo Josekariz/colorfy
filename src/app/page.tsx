47f83b1807b353bae25f3de15bd01b94a733cdd1
Joseph <sejokarizz@gmail.com>
2023-10-25 18:58:13 +0300

    color storage working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 "use client"
 import React, { useState } from 'react';
+import ColorCard from '@/components/ColorCard';
+
+
 const colorPattern = /#(?:[0-9a-fA-F]{3,6})|\b(red|blue|green|yellow|purple|pink|orange|black|white|gray)\b|rgba?\([^)]*\)|hsla?\([^)]*\)/gi;
 
 export default function Home() {
@@ -8,10 +11,17 @@ export default function Home() {
 
   const handleInputChange = (e) => {
     const inputText = e.target.value;
-    const tokens = Array.from(inputText.matchAll(colorPattern), match => match[0].trim());
-    
+    const tokens = Array.from(new Set(inputText.match(colorPattern))).map((match) =>
+      match.trim()
+    );
+
     setInputValue(inputText);
     setColorTokens(tokens);
+  };
+
+  const removeColor = (color) => {
+    setColorTokens((prevTokens) => prevTokens.filter((token) => token !== color));
+    setInputValue((prevInput) => prevInput.replace(color, ''));
   };
 
   const handleSearch = () => {
@@ -20,8 +30,8 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <div className='flex min-h-screen items-center'>
+    <>
+      <div className="flex items-center min-h-screen">
         <div className="mx-auto">
           <input
             type="text"
@@ -30,15 +40,22 @@ export default function Home() {
             value={inputValue}
             onChange={handleInputChange}
           />
-          <button
-            className="btn btn-outline btn-info max-w-xl"
-            onClick={handleSearch}
-          >
-            Search
+          <button className="btn btn-outline btn-info" onClick={handleSearch}>
+            Lets get Mixing
           </button>
         </div>
       </div>
-      
-    </div>
+
+      {colorTokens.length >= 1 && (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 p-4">
+          {colorTokens.map((token, index) => (
+            <ColorCard key={index} color={token} onRemove={() => removeColor(token)} />
+          ))}
+        </div>
+      )}
+    </>
   );
 }
+
+
+
