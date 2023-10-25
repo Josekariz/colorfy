1a65cd30f74c46757bd3a382e1891d00df7ca953
Joseph Macharia <sejokarizz@gmail.com>
2023-10-25 13:15:21 +0000

    color split

--- colorfy/src/app/page.tsx
+++ colorfy/src/app/page.tsx
@@ -1,19 +1,44 @@
+"use client"
+import React, { useState } from 'react';
+const colorPattern = /#(?:[0-9a-fA-F]{3,6})|\b(red|blue|green|yellow|purple|pink|orange|black|white|gray)\b|rgba?\([^)]*\)|hsla?\([^)]*\)/gi;
 
 export default function Home() {
-  return (
+  const [inputValue, setInputValue] = useState('');
+  const [colorTokens, setColorTokens] = useState([]);
 
+  const handleInputChange = (e) => {
+    const inputText = e.target.value;
+    const tokens = Array.from(inputText.matchAll(colorPattern), match => match[0].trim());
+    
+    setInputValue(inputText);
+    setColorTokens(tokens);
+  };
+
+  const handleSearch = () => {
+    console.log("Input Value:", inputValue);
+    console.log("Color Tokens:", colorTokens);
+  };
+
+  return (
     <div>
       <div className='flex min-h-screen items-center'>
         <div className="mx-auto">
-          <input type="text" placeholder="Type here" className="input input-bordered  max-w-xl " />
-          <button className="btn btn-outline btn-info max-w-xl">Search</button>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered max-w-xl"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          <button
+            className="btn btn-outline btn-info max-w-xl"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
         </div>
       </div>
       
     </div>
-
-    
-
-
-  )
+  );
 }
---
+++ colorfy/src/components/ColorCard.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ColorCard = ({ color, onRemove }) => {
+  return (
+    <div className="card w-40 min-h-16 bg-primary text-primary-content relative">
+      <button
+        className="absolute top-1 right-1 p-2 rounded-full bg-glass cursor-pointer"
+        onClick={() => onRemove(color)}
+      >
+        X
+      </button>
+      <div className="card-body">
+        <h2 className="card-title text-center">{color}</h2>
+      </div>
+    </div>
+  );
+}
+
+export default ColorCard;
+
+// {<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-6 gap-4 p-4">
+//           {colorTokens.map((token, index) => (
+//             <ColorCard key={index} color={token} onRemove={removeColor} />
+//           ))}
+//         </div>}
